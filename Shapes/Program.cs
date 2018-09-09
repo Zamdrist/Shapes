ac78bf69ddd1ea96409a528357f0c485e4111ad8
Steve <zamdrist@gmail.com>
2018-09-09 17:01:07 -0500

    Initial commit

---
+++ Shapes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			var rectangle = new Rectangle("Steve's Perfect Rectangle",144,Rectangle.PerfectSquareReturns.Perimeter);
+	        Console.WriteLine(rectangle.SerializedRectangle());
+	        Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Shapes/Shape.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Shapes
+{
+	[DataContract]
+	public class Shape
+    {
+	    [DataMember] public double Perimeter { get; set; }
+        [DataMember] public double Area { get; set; }
+	    [DataMember] public string ShapeName { get; set; }
+	    [DataMember] public string ShapeException { get; set; }
+
+    }
+}
