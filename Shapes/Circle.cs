e7abee91bfaabc8eac3e691e17f77aa6dd041d24
Steve <zamdrist@gmail.com>
2018-09-14 22:29:00 -0500

    Adding classes for circle and right triangle

---
+++ Shapes/Circle.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Shapes
+{
+	[DataContract]
+    public class Circle : Shape
+    {
+		[DataMember] public double Radius { get; set; }
+		[DataMember] public double Diameter { get; set; }
+    }
+}
---
+++ Shapes/RightTriangle.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace Shapes
+{
+    [DataContract]
+    public class RightTriangle : Shape
+    {
+		[DataMember] public double LegA { get; set; }
+		[DataMember] public double LegB { get; set; }
+		[DataMember] public double LegC { get; set; }
+    }
+}
