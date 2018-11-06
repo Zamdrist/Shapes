f04b7818792499fabcbc2dbfa6ca53b28ff065eb
Steve <zamdrist@gmail.com>
2018-11-05 22:32:49 -0600

    Adding rectangle controller

---
+++ ShapeApi/Controllers/RectangleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shapes;
+
+namespace ShapeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RectangleController : ControllerBase
+    {
+	    [HttpGet]
+	    public ActionResult<Shape> Get(
+		    string shapeName,
+		    double firstDimension,
+		    double secondDimension,
+		    Rectangle.RectangleDimensions rectangleDimensions)
+	    {
+			return new Rectangle(shapeName, firstDimension, secondDimension, rectangleDimensions);
+	    }
+    }
+}
