a476134bfc410d521718b98ef2c79c66cf228d9d
Steve <zamdrist@gmail.com>
2018-12-22 14:57:38 -0600

    Updated rectangle controller, added circle controller

---
+++ ShapeApi/Controllers/CircleController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shapes;
+
+namespace ShapeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CircleController : ControllerBase
+    {
+	    [HttpGet("{shapeName}/{dimension}/{circleDimensions}")]
+	    public ActionResult<Shape> Get(
+		    string shapeName,
+		    double dimension,
+		    Circle.CircleDimensions circleDimensions)
+	    {
+		    return new Circle(shapeName, dimension, circleDimensions);
+	    }
+    }
+}
