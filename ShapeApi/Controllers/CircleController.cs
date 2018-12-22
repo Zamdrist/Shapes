2b26e43409743c2e364d9fbbc5177de8cae4dfab
Steve <zamdrist@gmail.com>
2018-12-22 15:00:00 -0600

    Untabify

--- ShapeApi/Controllers/CircleController.cs
+++ ShapeApi/Controllers/CircleController.cs
@@ -7,13 +7,13 @@ namespace ShapeApi.Controllers
     [ApiController]
     public class CircleController : ControllerBase
     {
-	    [HttpGet("{shapeName}/{dimension}/{circleDimensions}")]
-	    public ActionResult<Shape> Get(
-		    string shapeName,
-		    double dimension,
-		    Circle.CircleDimensions circleDimensions)
-	    {
-		    return new Circle(shapeName, dimension, circleDimensions);
-	    }
+        [HttpGet("{shapeName}/{dimension}/{circleDimensions}")]
+        public ActionResult<Shape> Get(
+            string shapeName,
+            double dimension,
+            Circle.CircleDimensions circleDimensions)
+        {
+            return new Circle(shapeName, dimension, circleDimensions);
+        }
     }
 }
--- ShapeApi/Controllers/RectangleController.cs
+++ ShapeApi/Controllers/RectangleController.cs
@@ -7,25 +7,25 @@ namespace ShapeApi.Controllers
     [ApiController]
     public class RectangleController : ControllerBase
     {
-	    //https://localhost:44328/api/Rectangle/Sq/11/101/LengthArea
+        //https://localhost:44328/api/Rectangle/Sq/11/101/LengthArea
         [HttpGet("{shapeName}/{firstDimension}/{secondDimension}/{rectangleDimensions}")]
-	    public ActionResult<Shape> GetRectangle(
-		    string shapeName,
-		    double firstDimension,
-		    double secondDimension,
-		    Rectangle.RectangleDimensions rectangleDimensions)
-	    {
-		    return new Rectangle(shapeName, firstDimension, secondDimension, rectangleDimensions);
-	    }
+        public ActionResult<Shape> GetRectangle(
+            string shapeName,
+            double firstDimension,
+            double secondDimension,
+            Rectangle.RectangleDimensions rectangleDimensions)
+        {
+            return new Rectangle(shapeName, firstDimension, secondDimension, rectangleDimensions);
+        }
         //https://localhost:44328/api/Rectangle/PerfectSquare/MyPerf/10/Area
         [HttpGet("PerfectSquare/{shapeName}/{firstDimension}/{perfectSquareDimensions}")]
-	    public ActionResult<Shape> GetPerfectSquare(
-		    string shapeName,
-		    double firstDimension,
-		    Rectangle.PerfectSquare perfectSquareDimensions)
-	    {
-		    return new Rectangle(shapeName, firstDimension, perfectSquareDimensions);
+        public ActionResult<Shape> GetPerfectSquare(
+            string shapeName,
+            double firstDimension,
+            Rectangle.PerfectSquare perfectSquareDimensions)
+        {
+            return new Rectangle(shapeName, firstDimension, perfectSquareDimensions);
 
-	    }
+        }
     }
 }
